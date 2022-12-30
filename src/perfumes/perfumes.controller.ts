f984a06795f68333265ce81c5087e493abc33c9f
hanjoo0211 <hanjoo0211@gmail.com>
2022-12-30 17:06:37 +0900

    feat: add perfumes CRUD

--- src/perfumes/perfumes.controller.ts
+++ src/perfumes/perfumes.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { PerfumesService } from './perfumes.service';
+import { AddPerfumeDto, UpdatePerfumeDto } from './perfumes.dto';
 import { Perfumes } from './perfumes.entity';
 
 @Controller('perfumes')
@@ -14,5 +15,20 @@ export class PerfumesController {
     @Get(':id')
     getPerfumeById(@Param('id') id: number): Promise<Perfumes> {
         return this.perfumesService.getPerfumeById(id);
+    }
+
+    @Post()
+    addPerfume(@Body() addPerfumeDto: AddPerfumeDto): Promise<void> {
+        return this.perfumesService.addPerfume(addPerfumeDto);
+    }
+
+    @Put(':id')
+    updatePerfume(@Param('id') id: number, @Body() updatePerfumeDto: UpdatePerfumeDto): Promise<void> {
+        return this.perfumesService.updatePerfumeById(id, updatePerfumeDto);
+    }
+
+    @Delete(':id')
+    deletePerfume(@Param('id') id: number): Promise<void> {
+        return this.perfumesService.deletePerfumeById(id);
     }
 }
--- src/perfumes/perfumes.service.ts
+++ src/perfumes/perfumes.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AddPerfumeDto, UpdatePerfumeDto } from './perfumes.dto';
 import { Repository } from 'typeorm';
 import { Perfumes } from './perfumes.entity';
 
@@ -16,5 +17,17 @@ export class PerfumesService {
 
     getPerfumeById(id: number): Promise<Perfumes> {
         return this.perfumesRepository.findOneBy({ id });
+    }
+
+    async addPerfume(addPerfumeDto: AddPerfumeDto): Promise<void> {
+        await this.perfumesRepository.insert(addPerfumeDto);
+    }
+
+    async updatePerfumeById(id: number, updatePerfumeDto: UpdatePerfumeDto): Promise<void> {
+        await this.perfumesRepository.update(id, updatePerfumeDto);
+    }
+
+    async deletePerfumeById(id: number): Promise<void> {
+        await this.perfumesRepository.delete(id);
     }
 }
